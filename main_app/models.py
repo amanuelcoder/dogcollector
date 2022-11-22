8ec99e0a884d0c43ec8546b63ab4a30fbba0caa2
amanuelcoder <amanuel.mg@gmail.com>
2022-11-21 19:47:45 -0500

    first commit

---
+++ main_app/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+# Import the reverse function
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
+# Create your models here.
+class Dog(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    toys = models.ManyToManyField(Toy)
+
+def __str__(self):
+    return f'{self.name} ({self.id})'
+
+def get_absolute_url(self):
+    return reverse('detail', kwargs={'dog_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+
+  dog = models.ForeignKey(
+    Dog, 
+    on_delete=models.CASCADE
+    )
+
+def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+
+class Meta:
+    ordering = ['-date']
